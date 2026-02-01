985403fad2714469be2d9f6e62504fd95122302d
jylim26 <yejong98@gmail.com>
2026-02-01 23:40:06 +0900

    first init

---
+++ src/main/java/com/my/concert_u/ConcertUApplication.java
@@ -0,0 +1,13 @@
+package com.my.concert_u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertUApplication.class, args);
+	}
+
+}
